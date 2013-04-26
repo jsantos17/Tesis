8efbd653c00252475c9059811deed213777aa1e9
Juan Pablo Santos <santos.juanpablo@gmail.com>
2013-04-26 09:01:08 -0500

    Raw socket communication with device

--- gui/MeasureHandler.py
+++ gui/MeasureHandler.py
@@ -4,11 +4,15 @@ from lib.util.SweepType import SweepType
 from lib.util.SourceType import SourceType
 from lib.util.SourceMode import SourceMode
 from lib.SMUSweep import SMUSweep
-
+import socket
+import time
 
 class MeasureHandler(QtGui.QMainWindow):
 
     def handle(self, event, ui, params):
+        ip = ui.ipField.text()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip,1225))
         active = list()
         mapping = [{'combo': ui.smu1_combo, 'layout': ui.smu1_layout},
                    {'combo': ui.smu2_combo, 'layout': ui.smu2_layout},
@@ -25,13 +29,17 @@ class MeasureHandler(QtGui.QMainWindow):
             groupbox = layout.itemAt(2).widget()
             if "current" in combo.currentText().toLower():
                 if "sweep" in combo.currentText().toLower():
-                    ch = float(str(combo.objectName())[3:4])
+                    ch = int(str(combo.objectName())[3:4])
                     stop = float(groupbox.findChild(QtGui.QLineEdit, "val_stop_field").text())
                     start = float(groupbox.findChild(QtGui.QLineEdit, "val_inicio_field").text())
                     step = float(groupbox.findChild(QtGui.QLineEdit, "step_field").text())
                     compliance = float(groupbox.findChild(QtGui.QLineEdit, "compliance_field").text())
                     smu = SMUSweep('V1','I1',SourceMode.CURRENT, ch, SourceType.CURRENT, start, stop, step, compliance, sweep_type=SweepType.LINEAR)
-                    print smu.get_commands()
-                    print [ch, stop, start, step, compliance]
+                    for command in smu.get_commands():
+                        time.sleep(1)
+                        print command
+                        s.send(command)
+                    s.send('MD ME1')
+                    s.send('DO\'I1\'')  
             elif "voltage" in combo.currentText().toLower():
                 pass
