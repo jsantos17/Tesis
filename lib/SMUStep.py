027713ed53249863ebf806524f675701bdc0ab60
Juan Pablo Santos <santos.juanpablo@gmail.com>
2013-11-15 11:48:52 -0500

    SMU step fixed

--- lib/SMUStep.py
+++ lib/SMUStep.py
@@ -39,21 +39,21 @@ class SMUStep(SMUBase):
 
     def _get_var2_cmd(self):
         if self.source_type == SourceType.VOLTAGE:
-            template = "VP{start},{step},{steps},{compliance}"
+            template = "SS VP{start},{step},{steps},{compliance}"
         elif self.source_type == SourceType.CURRENT:
-            template = "IP{start},{step},{steps},{compliance}"
+            template = "SS IP{start},{step},{steps},{compliance}"
 
         command = template.format(start=self.start, step=self.step, steps=self.steps, 
                                  compliance=self.compliance)
         return command
 
     def _get_chan_cmd(self):
-        command = "CH{ch_number},'{voltage_name}','{current_name}',{source_mode},2".format(ch_number=self.ch_number,
+        command = "DE CH{ch_number},'{voltage_name}','{current_name}',{source_mode},2".format(ch_number=self.ch_number,
                                         voltage_name=self.voltage_name, current_name=self.current_name, source_mode=self.source_mode)
         return command
 
     def get_commands(self):
-        return ["DE", self._get_chan_cmd(), "SS", self._get_var2_cmd()]
+        return [self._get_chan_cmd(), self._get_var2_cmd()]
 
 
 class SMUStepConfigError(Exception):
