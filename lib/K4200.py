a5f48196c2c9d778b7baa174a4a954b98bd57ba1
Juan Pablo Santos <santos.juanpablo@gmail.com>
2013-11-15 11:48:31 -0500

    Fixed Instrument object

--- lib/K4200.py
+++ lib/K4200.py
@@ -4,14 +4,19 @@ from visa import instrument
 
 class K4200:
     
-    def __init__(self, ip)
+    def __init__(self, ip, port):
         self.ip = ip
-        self.k4200 = instrument("TCPIP::{ip}::INSTR".
-                            format(ip=ip))
+        self.k4200 = instrument("TCPIP::{ip}::{port}::SOCKET".
+                            format(ip=self.ip, port=self.port))
         self.smus = list()
     
-    def attach(smu):
+    def attach(self, smu):
         self.smus.append(smu)
+
+    def measure(self):
+        for commands in smus:
+            for command in smus.get_commands():
+                k4200.write(command)
 
     def __str__(self):
         pass
