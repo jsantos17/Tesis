b8c3bde056e4766c2f9667ad2d7380c8393e5b35
Juan Pablo Santos <santos.juanpablo@gmail.com>
2014-01-17 11:27:32 -0500

    Added routines to get data from device

--- lib/K4200.py
+++ lib/K4200.py
@@ -9,6 +9,7 @@ class K4200:
         self.ip = ip
         self.executor = SocketExecutor(ip, port)
         self.configured = False
+        self.has_measured = False
         self.smus = list()
     
     def attach(self, smu):
@@ -22,5 +23,16 @@ class K4200:
 
     def measure(self):
         self.executor.execute_command("MD ME1")
+        self.has_measured = True
 
+    def get_data(self, ch=1):
+        if has_measured:
+            template = "DO CH{ch}T"
+            cmd = template.format(ch=ch)
+            self.executor.execute_command(cmd)
+            return executor.get_data()
+        else:
+            raise NoDataError("No data to retrieve")
 
+class NotMeasuredYetError(Exception):
+    pass
