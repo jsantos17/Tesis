490353ddb77b2c59067caf193389cf4a5c52964a
Juan Pablo Santos <santos.juanpablo@gmail.com>
2014-02-09 16:19:24 -0500

    Close menu item

--- gui/MenuHandlers.py
+++ gui/MenuHandlers.py
@@ -1,9 +1,8 @@
 from utils import save_ui_file
 from utils import restore_ui_file
 from PyQt4.QtGui import QFileDialog
+from PyQt4.QtGui import QApplication 
 
-def handle_save(ui):
-    pass
     
 def handle_save_as(ui):
     file_name = QFileDialog.getSaveFileName(ui.centralwidget, "Nombre del archivo", "~/untitled.yml", "YAML files (*.yaml, *.yml)")
@@ -14,4 +13,4 @@ def handle_open(ui):
     restore_ui_file(ui, file_name)
 
 def handle_close(ui):
-    pass
+    QApplication.quit() 
