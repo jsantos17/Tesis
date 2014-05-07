1865bf513886a39da51bd784b43a03eb63c2b40b
Juan Pablo Santos <santos.juanpablo@gmail.com>
2014-05-07 18:12:24 -0500

    Transform admittance, reflectance and resistance

---
+++ lib/util/DataTransformers.py
@@ -0,0 +1,66 @@
+from numpy.linalg import inv
+
+def single_z_from_s(data):
+    s11 = data[0]
+    s12 = data[1]
+    s21 = data[2]
+    s22 = data[3]
+
+    delta_s = (1-s11)*(1-s22) - s12*s21
+    z0 = 50 # characteristing impendance of 50 ohms
+
+    z11 = (((1+s11)*(1-s22)+s12*s21)/delta_s)*z0
+    z12 = (2*s12*z0)/delta_s
+    z21 = (2*s21*z0)/delta_s
+    z22 = (((1-s11)*(1+s22)+s12*s21)/delta_s)*z0
+
+    return [z11, z12, z21, z22]
+
+def single_y_from_s(data):
+    inv(single_z_from_s(data)).tolist()
+
+def y_from_s(sdata):
+    ydata = []
+    y11_list = []
+    y12_list = []
+    y21_list = []
+    y22_list = []
+
+
+    for idx, d in enumerate(sdata):
+        s11 = sdata[idx][0]
+        s12 = sdata[idx][1]
+        s21 = sdata[idx][2]
+        s22 = sdata[idx][3]
+
+        Y = single_y_from_s([s11, s12, s21, s22])
+        y11_list.append(Y[0])
+        y12_list.append(Y[1])
+        y21_list.append(Y[2])
+        y22_list.append(Y[3])
+
+    ydata = [y11_list, y12_list, y21_list, y22_list]
+    return ydata
+
+def z_from_s(sdata):
+    zdata = []
+    z11_list = []
+    z12_list = []
+    z21_list = []
+    z22_list = []
+
+
+    for idx, d in enumerate(sdata):
+        s11 = sdata[idx][0]
+        s12 = sdata[idx][1]
+        s21 = sdata[idx][2]
+        s22 = sdata[idx][3]
+
+        Z = single_z_from_s([s11, s12, s21, s22])
+        z11_list.append(Z[0])
+        z12_list.append(Z[1])
+        z21_list.append(Z[2])
+        z22_list.append(Z[3])
+
+    zdata = [z11_list, z12_list, z21_list, z22_list]
+    return zdata
